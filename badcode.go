3f3e6a4b526567f0b40bf0e62ce2b18a436a846d
Aldi Hilman Ramadhani <aldi.hilman@tokopedia.com>
2021-12-02 21:44:17 +0700

    feat(badcode): new bad code

--- badcode.go
+++ badcode.go
@@ -18,22 +18,15 @@ func badcode(itemName string) (Item, error) {
 
 			if itemName, ok := some_function3(itemName); ok {
 
-				if itemName, ok := some_function1(itemName); ok {
-					if itemName, ok := some_function2(itemName); ok {
+				if itemName, ok := some_function4(itemName); ok {
 
-						if itemName, ok := some_function3(itemName); ok {
-							return Item{
-								Number: 1,
-								Active: itemName == "",
-							}, nil
-						} else {
-							return EmptyItem, errors.New("function 3 isn't ok")
-						}
-					} else {
-						return EmptyItem, errors.New("function 2 isn't ok")
-					}
+					return Item{
+						Number: 1,
+						Active: itemName == "",
+					}, nil
+
 				} else {
-					return EmptyItem, errors.New("function 1 isn't ok")
+					return EmptyItem, errors.New("function 4 isn't ok")
 				}
 
 			} else {
@@ -62,5 +55,9 @@ func some_function3(itemName string) (string, bool) {
 }
 
 func some_function4(itemName string) (string, bool) {
+	return itemName, true
+}
+
+func some_function5(itemName string) (string, bool) {
 	return itemName, true
 }
